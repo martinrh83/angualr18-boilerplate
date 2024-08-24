0d25de9f1d4a42e189f3f4b144044ad8123f9b23
martinrh83 <martinrh83@gmail.com>
2024-08-24 20:14:49 -0300

    Added routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'example',
+    loadChildren: () =>
+      import('./example/example.routes').then((m) => m.EXAMPLE_ROUTES),
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
